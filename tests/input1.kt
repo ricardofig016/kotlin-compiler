403dfc098e3d7d45848482cb6747bc2f830ca365
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2024-11-02 20:10:42 +0000

    added semicolons to the end of every statement in test1

--- tests/input1.kt
+++ tests/input1.kt
@@ -9,78 +9,78 @@ kotlinc input1.kt -include-runtime -d input1.jar ; java -jar input1.jar
 
 fun main() {
     // 1. Variable Declarations and Assignments
-    var a: Int = 10    // Mutable variable
-    val b: Double = 20.5    // Immutable variable
+    var a: Int = 10;    // Mutable variable
+    val b: Double = 20.5;    // Immutable variable
 
-    print("Initial values of a: " + a + " and b: " + b + "\n")
+    print("Initial values of a: " + a + " and b: " + b + "\n");
 
     // Assigning a new value to 'a'
-    a = 15
-    print("Updated value of a: " + a + "\n")
+    a = 15;
+    print("Updated value of a: " + a + "\n");
 
     // 2. Arithmetic Expressions
-    val sum: Double = a + b
-    val difference: Double = b - a
-    val product: Double = a * b
-    val quotient: Double = b / a
-    val remainder: Double = b % a
+    val sum: Double = a + b;
+    val difference: Double = b - a;
+    val product: Double = a * b;
+    val quotient: Double = b / a;
+    val remainder: Double = b % a;
 
-    print("Arithmetic expressions:\n")
-    print("Sum: " + sum  + "\n")
-    print("Difference: " + difference  + "\n")
-    print("Product: " + product + "\n")
-    print("Quotient: " + quotient + "\n")
-    print("Remainder: " + remainder + "\n")
+    print("Arithmetic expressions:\n");
+    print("Sum: " + sum  + "\n");
+    print("Difference: " + difference  + "\n");
+    print("Product: " + product + "\n");
+    print("Quotient: " + quotient + "\n");
+    print("Remainder: " + remainder + "\n");
 
     // 3. Boolean Expressions
-    val isAGreaterThanB: Boolean = a > b
-    val isALessThanB: Boolean = a < b
-    print("Boolean expressions:\n")
-    print("Is a greater than b? " + isAGreaterThanB + "\n")
-    print("Is a less than b? " + isALessThanB + "\n")
+    val isAGreaterThanB: Boolean = a > b;
+    val isALessThanB: Boolean = a < b;
+    print("Boolean expressions:\n");
+    print("Is a greater than b? " + isAGreaterThanB + "\n");
+    print("Is a less than b? " + isALessThanB + "\n");
 
     // 4. Reading input from the user
-    print("Enter your name: ")
-    val name: String = readln()
-    print("Hello, " + name + "!" + "\n")
+    print("Enter your name: ");
+    val name: String = readln();
+    print("Hello, " + name + "!" + "\n");
 
     // 5. Conditional Expressions (if-then-else)
-    print("Conditional expression:\n")
+    print("Conditional expression:\n");
     if (a > b) {
-        print("a is greater than b\n")
+        print("a is greater than b\n");
     } else if (a < b) {
-        print("a is less than b\n")
+        print("a is less than b\n");
     } else {
-        print("a is equal to b\n")
+        print("a is equal to b\n");
     }
 
     // 6. While Loop
-    var counter: Int = 0
-    print("While loop:\n")
+    var counter: Int = 0;
+    print("While loop:\n");
     while (counter < 5) {
-        print("Counter: " + counter + "\n")
+        print("Counter: " + counter + "\n");
         counter++  // Incrementing counter
     }
 
     // 7. Combining arithmetic and boolean expressions with a while loop
-    var x: Int = 5
-    print("Counting down with a while loop:\n")
+    var x: Int = 5;
+    print("Counting down with a while loop:\n");
     while (x > 0) {
-        print("x = " + x + "\n")
-        x--
+        print("x = " + x + "\n");
+        x--;
     }
 
     // 8. Chars
-    print("Chars:\n")
-    val c1: Char = 'a'
-    print("a: " + c1 + "\n")
-    val c2: Char = '\n'
-    print("\\n: " + c2 + "\n")
-    val c3: Char = '\\'
-    print("\\\\: " + c3 + "\n")
-    val c4: Char = ' '
-    print(" : " + c4 + "\n")
+    print("Chars:\n");
+    val c1: Char = 'a';
+    print("a: " + c1 + "\n");
+    val c2: Char = '\n';
+    print("\\n: " + c2 + "\n");
+    val c3: Char = '\\';
+    print("\\\\: " + c3 + "\n");
+    val c4: Char = ' ';
+    print(" : " + c4 + "\n");
 
     // Final message to test the compiler
-    print("Kotlin test complete!\n")
+    print("Kotlin test complete!\n");
 }
