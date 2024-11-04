076b1d3b65c1c6d247ebecb575db61a8f515ed71
Ricardo Figueiredo <ricardocastrofigueiredo@gmail.com>
2024-11-04 10:02:41 +0000

    fixed file paths to be relative to the root of the project (for cabal)

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,11 @@ import           Parser
 
 main :: IO ()
 main = do
-  input <- readFile "./../inputs/input1.kt"
+  input <- readFile "inputs/input1.kt"
   let tokens = alexScanTokens input
   let ast = parse tokens
   -- write to output file
-  let file = "../output.txt"
+  let file = "output.txt"
   writeFile file "Tokens:\n"
   appendFile file (show tokens)
   appendFile file "\n\n"
