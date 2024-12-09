90137289746892ddafd15d95e27de57cf3ec6065
Pbtm999 <pedro.batista.miguel9@gmail.com>
2024-12-09 21:04:50 +0000

    Liveness gen succ kill

--- inputs/input0.kt
+++ inputs/input0.kt
@@ -1,12 +1,9 @@
 fun main() {
-    var r: Int = 0;
-    var a: Int = 5;
-    var b: Int = 10;
+    var N: Int = 10;
+    var a: Int = 0;
 
-    while (b != 0) {
-        r = a%b;
-        a = b;
-        b = r;
+    while (a < N) {
+        a = 10;
     }
 
 }
--- src/Main.hs
+++ src/Main.hs
@@ -27,7 +27,7 @@ main = do
   print (checkAST ast)
   let (lowLevelCode, _) = runState (transAST ast) (0, 0)
   mapM_ (appendFile file . (++ "\n") . show) lowLevelCode
-  let graph = buildGraph program
+  let graph = buildGraph lowLevelCode
   mapM_ print graph
 
 printTokens :: Show a => a -> FilePath -> IO ()
